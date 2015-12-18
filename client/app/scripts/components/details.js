738787b2fd1cb1f1f96265dd0b897617bfe490da
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-12-18 18:02:45 +0100

    Details panel piles

--- client/app/scripts/components/details.js
+++ client/app/scripts/components/details.js
@@ -5,26 +5,39 @@ import NodeDetails from './node-details';
 export default class Details extends React.Component {
 
   render() {
+    const details = this.props.details.toIndexedSeq();
+    const pile = details.slice(0, -2);
+    const openDetails = details.slice(-2);
     return (
       <div className="details">
-        {this.props.details.toIndexedSeq().map((obj, index) => {
-          const offset = (420 + 12) * index;
+        {pile.map((obj, index) => {
+          const offset = (index + 1) * 8;
           const style = {
-            bottom: 48,
-            right: 36 + offset,
-            top: 24
+            right: 36 - offset,
+            top: 24 - offset
           };
-
           return (
-            <div id="details" style={style} key={obj.id}>
-              <div className="details-wrapper">
+            <div className="details-wrapper" style={style} key={obj.id}>
+              <div className="details-wrapper-row">
                 <NodeDetails details={obj.details} controlError={this.props.controlError}
                   controlPending={this.props.controlPending} nodes={this.props.nodes}
                   nodeId={obj.id} />
               </div>
+              <div className="details-wrapper-row details-wrapper-row-dummy" />
             </div>
           );
-        })};
+        })}
+        <div className="details-wrapper">
+          {openDetails.map((obj) => {
+            return (
+              <div className="details-wrapper-row" key={obj.id}>
+                <NodeDetails details={obj.details} controlError={this.props.controlError}
+                  controlPending={this.props.controlPending} nodes={this.props.nodes}
+                  nodeId={obj.id} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
