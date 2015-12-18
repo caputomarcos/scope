e17c99243e5154e3a6214136c7f770e0512f6bd3
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-12-18 14:51:22 +0100

    Stack details panel like cards

--- client/app/scripts/components/details.js
+++ client/app/scripts/components/details.js
@@ -6,16 +6,25 @@ export default class Details extends React.Component {
 
   render() {
     return (
-      <div id="details">
-        {this.props.details.toIndexedSeq().map(obj => {
+      <div className="details">
+        {this.props.details.toIndexedSeq().map((obj, index) => {
+          const offset = 10;
+          const style = {
+            bottom: 48 - index * offset,
+            right: 36 - index * offset,
+            top: 24 + index * offset
+          };
+
           return (
-            <div className="details-wrapper" key={obj.id}>
-              <NodeDetails details={obj.details} controlError={this.props.controlError}
-                controlPending={this.props.controlPending} nodes={this.props.nodes}
-                nodeId={obj.id} />
+            <div id="details" style={style} key={obj.id}>
+              <div className="details-wrapper">
+                <NodeDetails details={obj.details} controlError={this.props.controlError}
+                  controlPending={this.props.controlPending} nodes={this.props.nodes}
+                  nodeId={obj.id} />
+              </div>
             </div>
           );
-        })}
+        })};
       </div>
     );
   }
--- client/app/scripts/components/node-details/node-details-health.js
+++ client/app/scripts/components/node-details/node-details-health.js
@@ -4,9 +4,10 @@ import NodeDetailsHealthItem from './node-details-health-item';
 
 export default class NodeDetailsHealth extends React.Component {
   render() {
+    // TODO implement grouping metrics
     return (
       <div className="node-details-health">
-        {this.props.metrics && this.props.metrics.map(item => {
+        {this.props.metrics && this.props.metrics.slice(0, 3).map(item => {
           return <NodeDetailsHealthItem key={item.id} item={item} />;
         })}
       </div>
